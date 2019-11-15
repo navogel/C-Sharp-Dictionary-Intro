86e701397dfe7791c7312c98ceb76f2f5024e750
Nate V <navogel@gmail.com>
2019-11-15 10:25:00 -0600

    second commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionaryIntro
 {
@@ -6,7 +7,15 @@ namespace DictionaryIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //declare new dictionary variable.  same same but different to objects in JS.
+            Dictionary<string, int> cohortCount = new Dictionary<string, int>();
+
+            cohortCount.Add("Cohort 34", 20);
+            cohortCount.Add("Cohort 35", 14);
+            cohortCount.Add("Cohort 36", 28);
+            cohortCount.Add("Cohort 37", 28);
         }
+
+        cohortCount.
     }
 }
